417bf43667154bf31dc9d5f44e0847b2e2811ea4
Sisriksha Dhakal <binitadeuja66@gmail.com>
2026-04-28 18:51:14 -0700

    problem solv#1

--- supabase_client.py
+++ supabase_client.py
@@ -8,11 +8,11 @@ url: str = os.environ["SUPABASE_URL"]
 key: str = os.environ["SUPABASE_KEY"]
 def get_supabase() -> Client:
     return create_client(url, key)
-supabase: Client = create_client(url, key)
+# supabase: Client = create_client(url, key)
 
 # name = str(input("Enter todo"))
 
-response = supabase.table("todolist").select("*").execute().data
+# response = supabase.table("todolist").select("*").execute().data
 
 # print(response)
 
